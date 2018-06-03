ff6107cfd163ca28a777220b640ce8905561c6ac
NitRogeeen <miu1202.nitro@gmail.com>
2018-06-03 18:30:14 +0900

    コンポーネントのテンプレートを追加

---
+++ Electron/boilerplate/src/js/renderer/components/template/temp.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class Temp extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                Template.
+            </div>
+        );
+    }
+}
